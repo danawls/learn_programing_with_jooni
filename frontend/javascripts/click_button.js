778bdf10fc8d29a3717e795adaca11d273a5c012
danawls <woojin20110105@gmail.com>
2023-04-30 22:19:58 +0900

    add var

--- frontend/javascripts/click_button.js
+++ frontend/javascripts/click_button.js
@@ -1,3 +1,6 @@
-console.log(5 + 6)
-console.log(5 * 6)
-console.log(5 / 6)
+const jooni_sexy = 5;
+const jooni_cute = 6;
+
+console.log(jooni_sexy + jooni_cute)
+console.log(jooni_sexy * jooni_cute)
+console.log(jooni_sexy / jooni_cute)
