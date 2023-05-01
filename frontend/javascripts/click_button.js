08d32890f5ad45c57755dbb14049ca320d833e8f
danawls <woojin20110105@gmail.com>
2023-05-01 21:52:35 +0900

    upgrade my cal.

--- frontend/javascripts/click_button.js
+++ frontend/javascripts/click_button.js
@@ -1,23 +1,22 @@
 const cal = {
     add: function(a, b) {
-        console.log(a + b)
+        return (a + b);
     },
     sub: function(a, b) {
-        console.log(a - b)
+        return (a - b);
     },
     devide: function(a, b) {
-        console.log(a / b)
+        return (a / b);
     },
     multifly: function(a, b) {
-        console.log(a * b)
+        return (a * b);
     },
-    square: function(a,b) {
-        console.log(a ** b)
+    square: function(a, b) {
+        return (a ** b);
     },
-}
+};
 
-cal.add(1, 2)
-cal.sub(1, 2)
-cal.devide(1, 2)
-cal.multifly(1, 2)
-cal.square(1, 2)
+
+const allResults = [cal.add(1, 2), cal.devide(1, 2), cal.multifly(1, 2), cal.square(1, 2), cal.sub(1, 2)];
+
+console.log(allResults);
