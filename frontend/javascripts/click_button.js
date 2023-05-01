8f3d2eaf5291c5c661bf1146b6dda3219073453f
danawls <woojin20110105@gmail.com>
2023-05-01 21:17:11 +0900

    Update click_button.js

--- frontend/javascripts/click_button.js
+++ frontend/javascripts/click_button.js
@@ -1,7 +1,8 @@
 const player = {
     welcomeHello: function(name, age, severName, fat, sexy) {
-        console.log("hello " + name + "! you are " + age + " years old and your sever is " + severName +". and you are" + fat + " and " + sexy + "!")
+        console.log("hello " + name + "! you are " + age + " years old and your sever is " + severName +". and you are " + fat + " and " + sexy + "!")
     } 
 }
 
-player.welcomeHello("woojin", 12, "joonis Server", "n't fat", "you sexy");
+player.welcomeHello("woojin", 12, "joonis Server", "not fat", "you sexy");
+player.welcomeHello("jooni", 12, "woojins Server", "fat", "you are not sexy");
