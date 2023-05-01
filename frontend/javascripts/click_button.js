6f22adefd0c01ee5ac99afa6c9bf45386caa6d38
danawls <woojin20110105@gmail.com>
2023-05-01 22:01:15 +0900

    Update click_button.js

--- frontend/javascripts/click_button.js
+++ frontend/javascripts/click_button.js
@@ -16,6 +16,13 @@ const cal = {
     },
 };
 
+const addResult = cal.add(1, 2);
+const subResult = cal.sub(addResult, 2);
+const devideResult = cal.devide(subResult, 1);
+const multiflyResult = cal.multifly(devideResult, 5);
+const squareResult =  cal.square(multiflyResult, 2);
+
+console.log(addResult, subResult, devideResult, multiflyResult, squareResult);
 
 const allResults = [cal.add(1, 2), cal.devide(1, 2), cal.multifly(1, 2), cal.square(1, 2), cal.sub(1, 2)];
 
