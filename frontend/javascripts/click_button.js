ee2d1cf5408a77c5599ead264bc246d747bd773e
danawls <woojin20110105@gmail.com>
2023-05-03 22:53:49 +0900

    what the...... what's wrong...?

--- frontend/javascripts/click_button.js
+++ frontend/javascripts/click_button.js
@@ -4,8 +4,10 @@ if(isNaN(age) == true) {
     console.log("Please write a number");
 } else if(age < 19) {
     console.log("you can drink because you are less than 19 years old.");
-} else if(age >= 19 && age <= 50) {
+} else if(age >= 19 && age <= 50 && age < 100) {
     console.log("You can drink!");
+} else if(age <= 100) {
+    console.log("You CAN'T drinkkkkk!!!!");
 } else {
     console.log("You can drink but....Be careful! if you drink a many alchool, You can dye");
 }
