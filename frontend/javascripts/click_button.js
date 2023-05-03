9243fd4f38f80e2659cc67816ae220093bdc4f74
danawls <woojin20110105@gmail.com>
2023-05-03 22:38:27 +0900

    very simple if and else

--- frontend/javascripts/click_button.js
+++ frontend/javascripts/click_button.js
@@ -1,29 +1,5 @@
-const cal = {
-    add: function(a, b) {
-        return (a + b);
-    },
-    sub: function(a, b) {
-        return (a - b);
-    },
-    devide: function(a, b) {
-        return (a / b);
-    },
-    multifly: function(a, b) {
-        return (a * b);
-    },
-    square: function(a, b) {
-        return (a ** b);
-    },
-};
+const age = parseInt( prompt("How old are you?" ) );
 
-const addResult = cal.add(1, 2);
-const subResult = cal.sub(addResult, 2);
-const devideResult = cal.devide(subResult, 1);
-const multiflyResult = cal.multifly(devideResult, 5);
-const squareResult =  cal.square(multiflyResult, 2);
-
-console.log(addResult, subResult, devideResult, multiflyResult, squareResult);
-
-const allResults = [cal.add(1, 2), cal.devide(1, 2), cal.multifly(1, 2), cal.square(1, 2), cal.sub(1, 2)];
-
-console.log(allResults);
+if(isNaN(age) == true) {
+    console.log("Please write a number")
+}
