7ddcc1efebce0e1ca4412447fc75737d48bb2f23
danawls <woojin20110105@gmail.com>
2023-05-03 22:49:32 +0900

    very simple alchooool cal.

--- frontend/javascripts/click_button.js
+++ frontend/javascripts/click_button.js
@@ -1,5 +1,11 @@
 const age = parseInt( prompt("How old are you?" ) );
 
 if(isNaN(age) == true) {
-    console.log("Please write a number")
+    console.log("Please write a number");
+} else if(age < 19) {
+    console.log("you can drink because you are less than 19 years old.");
+} else if(age >= 19 && age <= 50) {
+    console.log("You can drink!");
+} else {
+    console.log("You can drink but....Be careful! if you drink a many alchool, You can dye");
 }
