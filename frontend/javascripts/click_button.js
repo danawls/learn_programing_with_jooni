4707af4ce0db4fbb050b1914159faa34f26bac55
danawls <woojin20110105@gmail.com>
2023-05-07 00:54:17 +0900

    my father is fucking bitch.
    He will have to be dye.

--- frontend/javascripts/click_button.js
+++ frontend/javascripts/click_button.js
@@ -1,4 +1,7 @@
 const hellos = document.querySelector(".hello:first-child h1");
 
 hellos.innerText = "This is spy";
-console.log(hellos);
+
+hellos.addEventListener("click", )
+
+console.dir(hellos.style.color);
