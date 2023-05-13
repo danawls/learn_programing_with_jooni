80335872db72eb29f867856b47a29f80d1155981
danawls <woojin20110105@gmail.com>
2023-05-14 01:25:00 +0900

    css in javascript 1

--- frontend/javascripts/click_button.js
+++ frontend/javascripts/click_button.js
@@ -1,40 +1,15 @@
-const title = document.querySelector(".hello:first-child h1");
+const h1 = document.getElementById("sexy");
 
-title.innerText = "This is spy";
+h1.style.cursor = "pointer";
+h1.style.fontSize = "100px";
 
-function handleClick() {
-    title.style.color = "blue";
+function handleTitleClick() {
+    if (h1.style.color === "blue") {
+        h1.style.color = "tomato";
+    } else {
+        h1.style.color = "blue";
+
+    }
 }
 
-function handleMouseEnter () {
-    title.style.color = "red";
-}
-
-function handleMouseLeave () {
-    title.style.color = "brown";
-}
-
-/** 
- * @access 오오 이 주석 신기방기하다아앙
-*/
-function handleWindowResize () {
-    document.body.style.backgroundColor= "azure";
-}
-
-function handleWindowCopy () {
-    alert ("copier!");
-}
-// 오프라인인 사용자에게는 알락으로 경고
-function handleOfflineUser () {
-    alert ("Please connect internet");
-}
-
-/** 1. 클릭했을 때 블루로 변경 2. 호버 상태에서 레드 3. 마우스가 떠나면 브라운으로 변경 */
-title.addEventListener("click", handleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleOfflineUser);
-
-console.dir(title.style.color);
+h1.addEventListener("click", handleTitleClick);
