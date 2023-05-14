e6c3b956ec968a7ae8d9963553d929cd0f1c92b7
danawls <woojin20110105@gmail.com>
2023-05-14 18:54:29 +0900

    subit event

--- frontend/javascripts/click_button.js
+++ frontend/javascripts/click_button.js
@@ -1,3 +1,10 @@
 const logInForm = document.querySelector("#login-form");
 const logInInput = logInForm.querySelector("#login-input");
 const logInButton = logInForm.querySelector("#login-button");
+
+function handleSubmitLogInButton (dororo) {
+    dororo.preventDefault();
+    console.log(dororo);
+}
+
+logInForm.addEventListener("submit", handleSubmitLogInButton);
