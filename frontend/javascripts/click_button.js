4335adde3cf1e2257bbf5e73ec4621573f632c5e
danawls <woojin20110105@gmail.com>
2023-05-14 17:48:22 +0900

    re-start

--- frontend/javascripts/click_button.js
+++ frontend/javascripts/click_button.js
@@ -1,17 +1,3 @@
-const h1 = document.getElementById("sexy");
-
-h1.style.cursor = "pointer";
-h1.style.fontSize = "100px";
-
-function handleTitleClick() {
-    const clickedClass = "active";
-    const font = "sexy-font";
-    h1.classList.toggle(clickedClass);
-    if (h1.classList.contains(clickedClass)) {
-        h1.innerText = "hello";
-    } else {
-        h1.innerText = "안녕";
-    }
-}
-
-h1.addEventListener("click", handleTitleClick);
+const logInForm = document.querySelector("#login-form");
+const logInInput = logInForm.querySelector("#login-input");
+const logInButton = logInForm.querySelector("#login-button");
