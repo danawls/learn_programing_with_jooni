1e64adf442b087ee5258075d44be5dacd18fbdf9
danawls <woojin20110105@gmail.com>
2023-05-15 22:08:01 +0900

    displat:none

--- frontend/javascripts/click_button.js
+++ frontend/javascripts/click_button.js
@@ -1,18 +1,12 @@
 const logInForm = document.querySelector("#login-form");
 const logInInput = logInForm.querySelector("#login-input");
 const logInButton = logInForm.querySelector("#login-button");
-const link = document.querySelector("a");
 
 
 function handleSubmitLogInButton (event) {
-    event.preventDefault()
+    event.preventDefault();
+    logInInput.classList.toggle("hide");
     console.log(logInInput.value);
 }
 
-function handleClickLink (event) {
-    event.preventDefault();
-    console.log(event);
-}
-
 logInForm.addEventListener("submit", handleSubmitLogInButton);
-link.addEventListener("click", handleClickLink);
