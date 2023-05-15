3d273209dbe75f8b4362fd9255ce0bbd9b416b70
danawls <woojin20110105@gmail.com>
2023-05-15 21:50:22 +0900

    preventdefault

--- frontend/javascripts/click_button.js
+++ frontend/javascripts/click_button.js
@@ -1,10 +1,18 @@
 const logInForm = document.querySelector("#login-form");
 const logInInput = logInForm.querySelector("#login-input");
 const logInButton = logInForm.querySelector("#login-button");
+const link = document.querySelector("a");
+
 
 function handleSubmitLogInButton (event) {
     event.preventDefault()
     console.log(logInInput.value);
 }
 
+function handleClickLink (event) {
+    event.preventDefault();
+    console.log(event);
+}
+
 logInForm.addEventListener("submit", handleSubmitLogInButton);
+link.addEventListener("click", handleClickLink);
