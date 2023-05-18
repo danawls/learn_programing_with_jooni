55f6976fd4576001e392bb15a457164eb17b8697
danawls <woojin20110105@gmail.com>
2023-05-18 21:47:53 +0900

    get user name

--- frontend/javascripts/click_button.js
+++ frontend/javascripts/click_button.js
@@ -1,13 +1,14 @@
 const logInForm = document.querySelector("#login-form");
 const logInInput = logInForm.querySelector("#login-input");
 const logInButton = logInForm.querySelector("#login-button");
-
+const greet = document.querySelector("#greet");
 
 function handleSubmitLogInButton (event) {
     event.preventDefault();
     const username = logInInput.value;
     logInForm.classList.add("hide");
-    console.log(username);
+    greet.innerText = "hello " + username + "!";
+    greet.classList.remove("hide");
 }
 
-logInForm.addEventListener("submit", handleSubmitLogInButton);
+window.addEventListener("resize", handleSubmitLogInButton);
