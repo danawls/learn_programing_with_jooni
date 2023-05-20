9ba772cbf54562c3a89f8fbbd201903cecd688cd
danawls <woojin20110105@gmail.com>
2023-05-21 00:06:24 +0900

    getitem and show that

--- frontend/javascripts/click_button.js
+++ frontend/javascripts/click_button.js
@@ -4,13 +4,14 @@ const logInButton = logInForm.querySelector("#login-button");
 const greet = document.querySelector("#greet");
 
 const HIDE_CLASSNAME = "hide";
+const USERNAME_KEY = "username";
 
 function handleSubmitLogInButton (event) {
     event.preventDefault();
     const username = logInInput.value;
     logInForm.classList.add(HIDE_CLASSNAME);
-    localStorage.setItem("username", username);
-    greet.innerText = `Hello ${username}!`;
+    localStorage.setItem(USERNAME_KEY, username);
+        greet.innerText = `Hello ${localStorage.getItem(USERNAME_KEY)}!`;
     greet.classList.remove(HIDE_CLASSNAME);
 }
 
@@ -18,6 +19,16 @@ function handleGreetClicked () {
     navigator.clipboard.writeText(logInInput.value);
 }
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-logInForm.addEventListener("submit", handleSubmitLogInButton);
-greet.addEventListener("click", handleGreetClicked);
+if(savedUsername === null) {
+    // Show the form
+    logInForm.classList.remove(HIDE_CLASSNAME);
+    logInForm.addEventListener("submit", handleSubmitLogInButton);
+} else {
+    // Show the greet
+    greet.classList.remove(HIDE_CLASSNAME);
+        greet.innerText = `Hello ${localStorage.getItem(USERNAME_KEY)}!`;
+    greet.classList.remove(HIDE_CLASSNAME);
+    greet.addEventListener("click", handleGreetClicked);
+}
