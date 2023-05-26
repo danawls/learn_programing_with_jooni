be25a8242db706ed3c7e005a5be13fbc25100f2d
danawls <woojin20110105@gmail.com>
2023-05-26 22:59:13 +0900

    d

--- frontend/javascripts/greeting.js
+++ frontend/javascripts/greeting.js
@@ -7,32 +7,32 @@ const HIDE_CLASSNAME = "hide";
 const USERNAME_KEY = "username";
 
 function handleSubmitLogInButton(event) {
-    const username = logInInput.value;
-    event.preventDefault();
-    logInForm.classList.add(HIDE_CLASSNAME);
-    localStorage.setItem(USERNAME_KEY, username);
-    paintGreet(username);
-    greet.addEventListener("click", copyName);
+  const username = logInInput.value;
+  event.preventDefault();
+  logInForm.classList.add(HIDE_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreet(username);
+  greet.addEventListener("click", copyName);
 }
 
 function paintGreet(username) {
-    greet.innerText = `Hello ${username}!`;
-    greet.classList.remove(HIDE_CLASSNAME);
+  greet.innerText = `Hello ${username}!`;
+  greet.classList.remove(HIDE_CLASSNAME);
 }
 
 function copyName() {
-    const copy = localStorage.getItem(USERNAME_KEY);
-    navigator.clipboard.writeText(copy);
+  const copy = localStorage.getItem(USERNAME_KEY);
+  navigator.clipboard.writeText(copy);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-if(savedUsername === null) {
-    // Show the form
-    logInForm.classList.remove(HIDE_CLASSNAME);
-    logInForm.addEventListener("submit", handleSubmitLogInButton);
+if (savedUsername === null) {
+  // Show the form
+  logInForm.classList.remove(HIDE_CLASSNAME);
+  logInForm.addEventListener("submit", handleSubmitLogInButton);
 } else {
-    // Show the greet
-    paintGreet(savedUsername);
-    greet.addEventListener("click", copyName);
+  // Show the greet
+  paintGreet(savedUsername);
+  greet.addEventListener("click", copyName);
 }
