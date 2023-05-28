54961c5dec39da3ece07e6884c3e1f3a1805d742
danawls <woojin20110105@gmail.com>
2023-05-28 15:09:34 +0900

    detail for mabile

--- javascripts/backgroud-img.js
+++ javascripts/backgroud-img.js
@@ -1,12 +1,29 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const desktopImages = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const phoneImages = ["3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg", "7.jpeg"];
 
-const chosenImages = images[Math.floor(Math.random() * images.length)];
+const desktopChosenImages =
+  desktopImages[Math.floor(Math.random() * desktopImages.length)];
+const phoneChosenImages =
+  phoneImages[Math.floor(Math.random() * phoneImages.length)];
 
 const BGImage = document.createElement("img");
 
-BGImage.src = `images/${chosenImages}`;
+window.onload = function (event) {
+  const innerWidth = window.innerWidth;
+  const innerHeight = window.innerHeight;
+  innerHeight >= innerWidth
+    ? (BGImage.src = `images/${phoneChosenImages}`)
+    : (BGImage.src = `images/${desktopChosenImages}`);
+};
+
+window.onresize = function (event) {
+  const innerHeight = window.innerHeight;
+  const innerWidth = window.innerWidth;
+  innerHeight >= innerWidth
+    ? (BGImage.src = `images/${phoneChosenImages}`)
+    : (BGImage.src = `images/${desktopChosenImages}`);
+};
+
 BGImage.classList.add("size");
 
 document.body.appendChild(BGImage);
-
-BGImage.classList.add("b-g-img");
